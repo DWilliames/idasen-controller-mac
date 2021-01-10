5d014cea680c4a8e004a39bba496dd91da036491
David Williames <david@williames.com>
2021-01-10 15:01:18 +1100

    Initial Commit

---
+++ Desk Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Desk Controller
+//
+//  Created by David Williames on 10/1/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
